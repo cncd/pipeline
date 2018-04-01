20fb2f4efd792fc8dafc53aec766c2edcfdf0bb0
Brad Rydzewski <brad.rydzewski@gmail.com>
2018-04-01 11:32:51 -0700

    add context to k8s

--- pipeline/backend/kubernetes/kubernetes.go
+++ pipeline/backend/kubernetes/kubernetes.go
@@ -1,6 +1,7 @@
 package kubernetes
 
 import (
+	"context"
 	"io"
 
 	"github.com/cncd/pipeline/pipeline/backend"
@@ -22,39 +23,39 @@ func New(namespace, endpoint, token string) backend.Engine {
 }
 
 // Setup the pipeline environment.
-func (e *engine) Setup(*backend.Config) error {
+func (e *engine) Setup(context.Context, *backend.Config) error {
 	// POST /api/v1/namespaces
 	return nil
 }
 
 // Start the pipeline step.
-func (e *engine) Exec(*backend.Step) error {
+func (e *engine) Exec(context.Context, *backend.Step) error {
 	// POST /api/v1/namespaces/{namespace}/pods
 	return nil
 }
 
 // DEPRECATED
 // Kill the pipeline step.
-func (e *engine) Kill(*backend.Step) error {
+func (e *engine) Kill(context.Context, *backend.Step) error {
 	return nil
 }
 
 // Wait for the pipeline step to complete and returns
 // the completion results.
-func (e *engine) Wait(*backend.Step) (*backend.State, error) {
+func (e *engine) Wait(context.Context, *backend.Step) (*backend.State, error) {
 	// GET /api/v1/watch/namespaces/{namespace}/pods
 	// GET /api/v1/watch/namespaces/{namespace}/pods/{name}
 	return nil, nil
 }
 
 // Tail the pipeline step logs.
-func (e *engine) Tail(*backend.Step) (io.ReadCloser, error) {
+func (e *engine) Tail(context.Context, *backend.Step) (io.ReadCloser, error) {
 	// GET /api/v1/namespaces/{namespace}/pods/{name}/log
 	return nil, nil
 }
 
 // Destroy the pipeline environment.
-func (e *engine) Destroy(*backend.Config) error {
+func (e *engine) Destroy(context.Context, *backend.Config) error {
 	// DELETE /api/v1/namespaces/{name}
 	return nil
 }
